d7618a2cb1f32a6998b19093c999244708f8aa39
naoki <urasoe5@gmail.com>
2019-02-28 04:50:22 +0000

    画像追加

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,5 +1,5 @@
 class ListController < ApplicationController
-  MANDARA_IDS = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, ]
+  MANDARA_IDS = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
   PAINTING_IDS = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, ]
   WORK_IDS = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 , ]
   
